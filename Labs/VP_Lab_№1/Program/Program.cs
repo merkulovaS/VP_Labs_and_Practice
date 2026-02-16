9d874b7f7852964a46fcb1345954a36c19fc1225
merkulovaS <s.m.merkulova@gmail.com>
2026-02-16 21:56:26 +0300

    Add files via upload

---
+++ Labs/VP_Lab_№1/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserLibrary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите размер массива: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] originalArray = new int[size];
+
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Элемент {i + 1}: ");
+                originalArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"\nИсходный массив: {Array.ToString(originalArray)}");
+
+            var result = Array.SplitByParity(originalArray);
+
+            Console.WriteLine($"Чётные числа (отсортированы): {Array.ToString(result.EvenNumbers)}");
+            Console.WriteLine($"Нечётные числа (отсортированы): {Array.ToString(result.OddNumbers)}");
+
+        }
+    }
+}
---
+++ Labs/VP_Lab_№1/VP_Lab_№1/Array.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace UserLibrary
+{
+    public static class Array
+    {
+        public static (int[] EvenNumbers, int[] OddNumbers) SplitByParity(int[] sourceArray)
+        {
+            if (sourceArray == null || sourceArray.Length == 0)
+            {
+                return (new int[0], new int[0]);
+            }
+
+            int evenCount = 0;
+            int oddCount = 0;
+
+            foreach (int number in sourceArray)
+            {
+                if (number % 2 == 0)
+                    evenCount++;
+                else
+                    oddCount++;
+            }
+
+            int[] evenNumbers = new int[evenCount];
+            int[] oddNumbers = new int[oddCount];
+
+            int evenIndex = 0;
+            int oddIndex = 0;
+
+            foreach (int number in sourceArray)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers[evenIndex] = number;
+                    evenIndex++;
+                }
+                else
+                {
+                    oddNumbers[oddIndex] = number;
+                    oddIndex++;
+                }
+            }
+
+            SortArray(evenNumbers);
+            SortArray(oddNumbers);
+
+            return (evenNumbers, oddNumbers);
+        }
+
+        private static void SortArray(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static string ToString(int[] array)
+        {
+            if (array == null || array.Length == 0)
+                return "[]";
+
+            string result = "[";
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+                if (i < array.Length - 1)
+                    result += ", ";
+            }
+            result += "]";
+            return result;
+        }
+    }
+}
